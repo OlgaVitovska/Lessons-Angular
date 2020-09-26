4cafbb5b1ab1a0d18c5f7cbee2992728cd52d4e8
legende0512@gmail.com <legende0512@gmail.comgit config --global user.email legende0512@gmail.com>
2020-09-26 19:23:39 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'Lesson';
+  numberLesson = 1;
+  tasks = ['intro', 'start project', 'build', 'homework'];
+  posts = [{
+    title: 'post 1',
+    text: 'test post 1'
+  }, {
+    title: 'post 2',
+    text: 'test post 2'
+  }, {
+    title: 'post 3',
+    text: 'test post 3'
+  }]
+  state = false;
+  
+  dropdown() {
+    this.state = !this.state;
+  }
+    
+}
